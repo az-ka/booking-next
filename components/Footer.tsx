143bb7d87f9832fbe5116d899242fd03b29fea76
Azka <fadlian.dev@gmail.com>
2023-07-10 17:52:33 +0700

    Update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
                     <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
                         © 2023 Samsi Diesel —
                         <a
-                            href="https://twitter.com/knyttneve"
+                            href="#"
                             className="text-gray-600 ml-1"
                             rel="noopener noreferrer"
                             target="_blank"
